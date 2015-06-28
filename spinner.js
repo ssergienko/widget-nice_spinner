b43055572ea2d522afe61a8e882e7b227fc78b13
ssergienko <sergey.s.sergienko@gmail.com>
2015-06-28 19:29:48 +0300

    Create

---
+++ spinner.js
@@ -0,0 +1,27 @@
+﻿window.widgets = window.widgets || {};
+window.widgets.spinner = (function () {
+	return {
+		wait: function (options) {
+			options = options || {};
+			var $el = $(options.el || 'body');
+			var $loader = $('<div class="loader-wrapper"><div class="loader"></div></div>');
+			if (options.xhr) {
+				var timeout = setTimeout(function () {
+					$el.append($loader);
+				}, 200);
+				options.xhr.done(function () {
+					clearTimeout(timeout);
+					$loader.remove();
+				});
+			} else {				
+				$el.append($loader);
+			}
+			return {
+				destroy: function() {
+					$loader.remove();
+				},
+				$el: $loader
+			};
+		}
+	};
+})();
